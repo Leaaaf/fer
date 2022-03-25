62d4a07fd918dc3dfa0c81d5cdd421c7fc0df178
Leaaaf <alessandro.foglia@outlook.it>
2022-03-25 09:41:45 +0100

    feat: uploaded real model and modified handler to test simple local requests

--- aws-serverless/fer-sam-app/app/app.py
+++ aws-serverless/fer-sam-app/app/app.py
@@ -1,29 +1,36 @@
 import base64
 import json
-import numpy as np
-import tensorflow as tf
+# import numpy as np
+# import tensorflow as tf
 
-from PIL import Image
-from io import BytesIO
+# from PIL import Image
+# from io import BytesIO
 
 
-model_file = '/opt/ml/model'
-model = tf.keras.models.load_model(model_file)
-
+# model_file = '/opt/ml/model'
+# model = tf.keras.models.load_model(model_file)
+"""
+Per utilizzare Tensorflow inserire la dipendenza nel file 
+requirements.txt ( tensorflow==2.8.0 ) prima della build
+"""
 
 def lambda_handler(event, context):
-    image_bytes = event['body'].encode('utf-8')
-    image = Image.open(BytesIO(base64.b64decode(image_bytes))).convert(mode='L')
-    image = image.resize((28, 28))
 
-    probabilities = model(np.array(image).reshape(-1, 28, 28, 1))
-    label = np.argmax(probabilities)
+    print("*** lambda_handler ***")
+    print("Body: ", event.get('Body', None))
+
+    # image_bytes = event['body'].encode('utf-8')
+    # image = Image.open(BytesIO(base64.b64decode(image_bytes))).convert(mode='L')
+    # image = image.resize((28, 28))
+
+    # probabilities = model(np.array(image).reshape(-1, 28, 28, 1))
+    # label = np.argmax(probabilities)
 
     return {
         'statusCode': 200,
         'body': json.dumps(
             {
-                "predicted_label": int(label),
+                "predicted_label": event,
             }
         )
     }
