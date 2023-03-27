9bd475b07f56c6cf7586d0f08def7aa19d0137ec
Leaaaf <alessandro.foglia@outlook.it>
2023-03-28 00:00:18 +0200

    reshape input image

--- aws-serverless/fer-sam-app/app/app.py
+++ aws-serverless/fer-sam-app/app/app.py
@@ -11,12 +11,10 @@ model_file = '/opt/ml/model'
 model = tf.keras.models.load_model(model_file)
 
 def lambda_handler(event, context):
-
     image_bytes = event['body'].encode('utf-8')
     image = Image.open(BytesIO(base64.b64decode(image_bytes))).convert(mode='L')
-    image = image.resize((28, 28))
 
-    probabilities = model(np.array(image).reshape(-1, 28, 28, 1))
+    probabilities = model(np.array(image).reshape(None, 48, 48, 1))
     label = np.argmax(probabilities)
 
     return {
